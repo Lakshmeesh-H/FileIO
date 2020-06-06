41e7035c1c4524c5e1af0d8832291a52826d33e1
Lakshmeesh-H <lakshmeesh.h@gmail.com>
2020-06-06 09:27:50 +0530

    File Modes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-my_file = open('test.txt')
+# my_file = open('test.txt')
 # print(my_file.read())
 # my_file.seek(0)
 # print(my_file.read())
 # print(my_file.readline())
-print(my_file.readlines())
-my_file.close()
+# print(my_file.readlines())
+# my_file.close()
+
+# with open('test.txt', mode='a') as my_file:
+#     text = my_file.write('Hi Its Me again!')
+    
